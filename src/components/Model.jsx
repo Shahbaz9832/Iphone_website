9b333d4eb836db9dd2254eb1852048c2be3b50b8
Shahbaz Ansari <116244599+Shahbaz9832@users.noreply.github.com>
2025-07-08 19:41:06 +0530

    Update Model.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap";
-import ModelView from "./ModelView";
+import ModelView from "./src/components/Model.jsx";
 import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 
